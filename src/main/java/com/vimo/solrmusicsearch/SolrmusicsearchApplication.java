705e6181686cfaed05ea773d7fb3abc04c292eec
murshidmac <mh.murshidco.uk@outlook.com>
2019-07-21 21:10:47 +0530

    intial commit

---
+++ src/main/java/com/vimo/solrmusicsearch/SolrmusicsearchApplication.java
@@ -0,0 +1,13 @@
+package com.vimo.solrmusicsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrmusicsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrmusicsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vimo/solrmusicsearch/SolrmusicsearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vimo.solrmusicsearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SolrmusicsearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
